fbbccff14a3d2a6839a598d043a9ca3774e4b22f
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-03-26 14:33:59 +0600

    dynamic nested routing added

---
+++ routing-basics/src/app/products/[productId]/page.tsx
@@ -0,0 +1,6 @@
+const page = async ({ params }: { params: Promise<{ productId: string }> }) => {
+  const productId = (await params).productId;
+  return <h1>Details about product {productId} </h1>;
+};
+
+export default page;
