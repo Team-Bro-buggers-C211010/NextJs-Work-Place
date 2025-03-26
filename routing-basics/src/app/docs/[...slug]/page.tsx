7dac372ba754dc75aac624da5dad5bf31def9dd0
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-03-26 14:09:02 +0600

    Initial commit with Next.js projects

---
+++ routing-basics/src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,8 @@
+// here [...slug] is a catch-all route that will match /docs, /docs/a, /docs/a/b, and so on.
+export default function Docs() {
+    return (
+        <div>
+        <h1>Docs Page</h1>
+        </div>
+    );
+}
