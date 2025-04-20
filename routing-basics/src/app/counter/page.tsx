02398d40c4c823264194343d4a4cde4c38495c4b
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-04-20 22:59:18 +0600

    For client side pages handle metadata

--- routing-basics/src/app/counter/page.tsx
+++ routing-basics/src/app/counter/page.tsx
@@ -1,19 +1,13 @@
-"use client";
-
-import { useState } from "react";
-
-// export const metadata = {  
-//     title: "Counter",
-// }
+import Counter from './counter';
+export const metadata = {  
+    title: "Counter",
+}
 
 // Error:   × You are attempting to export "metadata" from a component marked with "use client", which is disallowed. Either remove the export, or the "use client" directive. Read more: https://nextjs.org/
-export default function Counter() {
-  const [count, setCount] = useState(0);
+export default function CounterPage() {
   return (
     <div>
-      <h1>Counter Page</h1>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <Counter/>
     </div>
   );
 }
