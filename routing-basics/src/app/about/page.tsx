5e8d00cd1afc0b277d99aff816fda28e5de93ded
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-04-20 21:37:58 +0600

    static metadata object configuration to change page-wise title for about page

---
+++ routing-basics/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "About by Shovan", // title of the page using static metadata
+}
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
