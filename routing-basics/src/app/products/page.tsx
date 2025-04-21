f948eef1a7384ea2c3fd927cff6645bfbf5cce9d
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-04-21 13:22:21 +0600

    replace attribute in Link

--- routing-basics/src/app/products/page.tsx
+++ routing-basics/src/app/products/page.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
+
 export default function Product() {
   return (
     <div>
         <h1>Products Page</h1>
         <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
+            <Link href={`/products/1`}>Product 1</Link>
+            <Link href={`/products/2`}>Product 2</Link>
+            <Link href={`/products/3`} replace >Product 3</Link>
+            {/* replace is used to replace the current history entry with the new one, instead of adding a new entry to the history stack. */}
         </ul>
     </div>
   )
