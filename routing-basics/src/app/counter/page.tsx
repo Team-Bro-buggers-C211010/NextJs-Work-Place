26abc92e1971678ad1a974913ee84b2d9297c6a3
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-04-21 13:13:31 +0600

    all about title metadata

--- routing-basics/src/app/counter/page.tsx
+++ routing-basics/src/app/counter/page.tsx
@@ -1,6 +1,8 @@
 import Counter from './counter';
 export const metadata = {  
-    title: "Counter",
+    title: {
+      absolute: 'Counter', // By using this the root template is not used
+    },
 }
 
 // Error:   × You are attempting to export "metadata" from a component marked with "use client", which is disallowed. Either remove the export, or the "use client" directive. Read more: https://nextjs.org/
