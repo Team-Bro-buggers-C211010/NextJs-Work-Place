d20542b104e87c29ad1214e7bd53b43b8f2da553
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-07-06 16:42:08 +0600

    recover from errors from both reload the client and server

--- routing-basics/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing-basics/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,36 @@
 "use client";
 
-export default function ErrorBoundary() {
-    return <div className="text-4xl text-red-500 font-extrabold text-center">Error in reviewId</div>;
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+// reset is rerender the client component
+// error is the error that was thrown in the server component
+// for rerender in server component, we can use `useRouter()` and startTransition
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+  return (
+    <div>
+      <div className="text-center">
+        <p className="text-4xl text-red-500 font-extrabold">{error.message}</p>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={reload}
+        >
+          Try again
+        </button>
+      </div>
+    </div>
+  );
 }
