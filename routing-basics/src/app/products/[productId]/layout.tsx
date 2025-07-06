070f1533cf60001b6448eb8c7d4e711eb883bba4
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-07-06 16:55:03 +0600

    error handle for layout file

--- routing-basics/src/app/products/[productId]/layout.tsx
+++ routing-basics/src/app/products/[productId]/layout.tsx
@@ -1,4 +1,17 @@
+const getRandomInt = (count: number) => {
+  return Math.floor(Math.random() * count) + 1; // get random number between 1 and count
+};
 export default function ProductDetailsLayout({ children }: { children: React.ReactNode }) {
+    // Simulating a random error for demonstration purposes
+    // random error in layout can not be handle by same folder error boundary
+    // it will be handled by the parent error boundary
+    // so we can use the error boundary in the parent folder i.e. products/error.tsx
+    // or we can use the error boundary in the root folder i.e. app/error.tsx
+    
+    const randomInt = getRandomInt(2);
+    if (randomInt === 2) {
+        throw new Error("Random Error for product details layout");
+    }
     return (
         <div>
             {children}
