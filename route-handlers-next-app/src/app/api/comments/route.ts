387b98e983c6ff02a9b7212bede079c28db3cd92
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-07-11 22:11:02 +0600

    basic route handler of GET and POST req.

---
+++ route-handlers-next-app/src/app/api/comments/route.ts
@@ -0,0 +1,21 @@
+import { comments } from "./data";
+
+
+export async function GET() {
+    return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+    const { text } = await request.json();
+    const newComment = {
+        id: comments.length + 1,
+        text,
+    };
+    comments.push(newComment);
+    return new Response(JSON.stringify(newComment), {
+        status: 201,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
