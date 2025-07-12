96ca0fd4436d3d613d33bf016bd618d5ac502b03
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-07-12 14:38:27 +0600

    cookie handler in many ways

--- route-handlers-next-app/src/app/api/profile/route.ts
+++ route-handlers-next-app/src/app/api/profile/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest){
@@ -8,9 +8,20 @@ export async function GET(request: NextRequest){
     const headersList = await headers();
     console.log(headersList.get("Authorization"));
 
+    // first way to get cookies
+    const theme = request.cookies.get("theme");
+    console.log("Theme from cookies:", theme);
+
+    // second way to set, and get cookies
+    const cookieStore = await cookies();
+    cookieStore.set("resultsPerPage", "10");
+    console.log("Results per page from cookies:", cookieStore.get("resultsPerPage"));
+
     return new Response("<h1>Hello from GET in profile route</h1>", {
         headers: {
-            "Content-Type": "text/html"
+            "Content-Type": "text/html",
+            // (first approach) Example of setting a cookie in the response
+            "Set-Cookie": "theme=dark"
         }
     });
 }
