f56cd1733202b538c59e8afb1f2549620628181c
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-07-12 13:46:00 +0600

    headers in Route Handler

---
+++ route-handlers-next-app/src/app/api/profile/route.ts
@@ -0,0 +1,16 @@
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest){
+    // const requestHeaders = new Headers(request.headers);
+    // console.log(requestHeaders.get("Authorization"));
+
+    const headersList = await headers();
+    console.log(headersList.get("Authorization"));
+
+    return new Response("<h1>Hello from GET in profile route</h1>", {
+        headers: {
+            "Content-Type": "text/html"
+        }
+    });
+}
