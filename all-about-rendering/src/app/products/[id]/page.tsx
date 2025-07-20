6f0094085ce22330b1e7fe585b370d46dfa76d84
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-07-20 21:14:24 +0600

    generateStaticParams for pre-render dynamic routes

--- all-about-rendering/src/app/products/[id]/page.tsx
+++ all-about-rendering/src/app/products/[id]/page.tsx
@@ -1,3 +1,11 @@
+export async function generateStaticParams() {
+    return [
+        { id: "1" },
+        { id: "2" },
+        { id: "3" }
+    ];
+}
+
 export default async  function ProductPage({ params }: {params: Promise<{ id: string }>} ) {
     const { id } = await params;
 
