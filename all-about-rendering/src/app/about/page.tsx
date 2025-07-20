065538c50e0d3d89425c78beb2d71b29d81a2ec0
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-07-20 20:30:59 +0600

    dynamic rendering

--- all-about-rendering/src/app/about/page.tsx
+++ all-about-rendering/src/app/about/page.tsx
@@ -1,5 +1,9 @@
-export default function About() {
-    console.log("About page rendered");
+import { cookies } from "next/headers";
+
+export default async function About() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme");
+  console.log("Current theme:", theme?.value);
   return (
     <div>
       <h1>About page load on: {new Date().toLocaleString()}</h1>
