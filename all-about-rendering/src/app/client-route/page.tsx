0cdbe7c0b2d25839fcd6316b1a55ebf52c752218
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-07-21 18:49:57 +0600

    client-only package for prevent it using in server component

--- all-about-rendering/src/app/client-route/page.tsx
+++ all-about-rendering/src/app/client-route/page.tsx
@@ -2,14 +2,16 @@
 
 import { ImageSlider } from "@/components/ImageSlider";
 import { useTheme } from "@/components/theme-provider";
+import { ClientSideFunction } from "@/utils/client-utils";
 
 export default function ClientRoute() {
 
     const theme = useTheme();
+    const result = ClientSideFunction();
     // const result = serverSideFunction();
     return (
         <div>
-            <h1 style={{ color: theme.colors.primary }}>Client Route</h1>
+            <h1 style={{ color: theme.colors.primary }}>Client Route {result}</h1>
             <ImageSlider />
         </div>
     );
--- all-about-rendering/src/app/server-route/page.tsx
+++ all-about-rendering/src/app/server-route/page.tsx
@@ -1,8 +1,10 @@
 import { ImageSlider } from "@/components/ImageSlider";
 import { serverSideFunction } from "@/utils/server-utils";
+import { ClientSideFunction } from "@/utils/client-utils";
 
 export default function ServerRoute() {
     const result = serverSideFunction();
+    // const clientResult = ClientSideFunction();
     return (
         <>
             <h1>Server Route {result}</h1>
