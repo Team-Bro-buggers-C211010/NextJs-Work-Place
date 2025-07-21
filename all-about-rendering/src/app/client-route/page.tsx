da292a0a00ccc7cbfd5a7663efe13ba786c8f959
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-07-21 18:28:43 +0600

    contextProvider usage

--- all-about-rendering/src/app/client-route/page.tsx
+++ all-about-rendering/src/app/client-route/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { ImageSlider } from "@/components/ImageSlider";
+import { useTheme } from "@/components/theme-provider";
 
 export default function ClientRoute() {
+
+    const theme = useTheme();
     // const result = serverSideFunction();
     return (
         <div>
-            <h1>Client Route</h1>
+            <h1 style={{ color: theme.colors.primary }}>Client Route</h1>
             <ImageSlider />
         </div>
     );
--- all-about-rendering/src/app/layout.tsx
+++ all-about-rendering/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Link href={"/dashboard"}>Dashboard</Link>
-        {children}
-      </body>
+      <ThemeProvider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Link href={"/dashboard"}>Dashboard</Link>
+          {children}
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
